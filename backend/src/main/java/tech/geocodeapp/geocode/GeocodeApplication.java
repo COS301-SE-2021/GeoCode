49abd37763600c5032c4da06ab65f4c8a97fb2bb
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-05-28 16:40:29 +0200

    Generated Spring backend

---
+++ backend/src/main/java/tech/geocodeapp/geocode/GeocodeApplication.java
@@ -0,0 +1,13 @@
+package tech.geocodeapp.geocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocodeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tech/geocodeapp/geocode/GeocodeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.geocodeapp.geocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
