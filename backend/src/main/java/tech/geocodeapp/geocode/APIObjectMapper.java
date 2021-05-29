9af8347f227863d42300dd0432f06b3dc0653931
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-05-29 16:27:28 +0200

    Implement application.properties

--- backend/src/main/java/tech/geocodeapp/geocode/APIObjectMapper.java
+++ backend/src/main/java/tech/geocodeapp/geocode/APIObjectMapper.java
@@ -2,7 +2,7 @@ package tech.geocodeapp.geocode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tech.geocodeapp.geocode.User.Service.UserInterfaceServiceImpl.UserServiceImpl;
+import tech.geocodeapp.geocode.User.Service.UserServiceImpl;
 
 /** This class handles mappings between API request objects and system objects
  *  called by the subsystems.
--- backend/src/main/java/tech/geocodeapp/geocode/User/Controller/UserController.java
+++ backend/src/main/java/tech/geocodeapp/geocode/User/Controller/UserController.java
@@ -5,14 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import tech.geocodeapp.geocode.APIObjectMapper;
-import tech.geocodeapp.geocode.User.Service.UserInterfaceServiceImpl.UserServiceImpl;
+import tech.geocodeapp.geocode.User.Service.*;
 
 /**
  * This class implements the functionality of the UserAPI interface.
  */
 @CrossOrigin("*")
 @RestController
-public class UserController implements UserApi {//TODO: generate UserApi
+public class UserController {//TODO: generate UserApi
     @Autowired
     private UserServiceImpl userService;
 
