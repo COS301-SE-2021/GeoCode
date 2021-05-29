50b4dfbf08ba463c46e09b1b1bbd05563fcf9dcd
Liam <nitron.biohazard@gmail.com>
2021-05-29 16:58:11 +0200

    Added Spring tags.

--- backend/src/main/java/tech/geocodeapp/geocode/Collectable/Model/Collectable.java
+++ backend/src/main/java/tech/geocodeapp/geocode/Collectable/Model/Collectable.java
@@ -1,10 +1,23 @@
 package tech.geocodeapp.geocode.Collectable.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+@Entity
 public class Collectable {
+
     //A link to the image location of the collectable
     private String image;
+
+    @Id
     //The unique name of a collectable
     private String name;
+
+    //Use ORDINAL to lower storage space usage
+    //TODO: Look into options that don't break data set if Enums in Rarity are changed
+    @Enumerated(EnumType.ORDINAL)
     //Enum set to the rarity of a collectable
     private Rarity rarity;
 
