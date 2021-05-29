dd423b2b5cc6b869c8c37ceecb53ec430d845339
Caleb Johnstone <u19030119@tuks.co.za>
2021-05-29 18:25:30 +0200

    initial createCollectable service implementation

--- backend/src/main/java/tech/geocodeapp/geocode/Collectable/Service/CollectableServiceImpl.java
+++ backend/src/main/java/tech/geocodeapp/geocode/Collectable/Service/CollectableServiceImpl.java
@@ -2,6 +2,7 @@ package tech.geocodeapp.geocode.Collectable.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.geocodeapp.geocode.Collectable.Model.Collectable;
 import tech.geocodeapp.geocode.Collectable.Repository.CollectableRepository;
 import tech.geocodeapp.geocode.Collectable.Request.CreateCollectableRequest;
 import tech.geocodeapp.geocode.Collectable.Request.GetCollectablesRequest;
@@ -9,6 +10,7 @@ import tech.geocodeapp.geocode.Collectable.Response.CreateCollectableResponse;
 import tech.geocodeapp.geocode.Collectable.Response.GetCollectablesResponse;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 /**
  * This class implements the UserService interface
@@ -25,17 +27,32 @@ public class CollectableServiceImpl implements CollectableService {
     @Transactional
     public CreateCollectableResponse createCollectable(CreateCollectableRequest request){
         if (request == null) {
-            return new CreateCollectableResponse();//TODO: pass parameters once class done
+            return new CreateCollectableResponse(false, "The CreateCollectableRequest object was null");
         }
 
-        return null;
+        Optional<Collectable> collectableName = collectableRepository.getCollectableByName(request.getName());
+
+        if(collectableName.isPresent()){
+            return new CreateCollectableResponse(false, "There already exists a Collectable with that name");
+        }
+
+        Collectable newCollectable = new Collectable(request.getImage(), request.getName(), request.getRarity());
+        Collectable checkIfSaved = collectableRepository.save(newCollectable);
+
+        if(checkIfSaved != newCollectable){
+            return new CreateCollectableResponse(false, "The Collectable was not saved.");
+        }else{
+            return new CreateCollectableResponse(true, "The Collectable was successfully created.");
+        }
     }
 
     @Transactional
     public GetCollectablesResponse getCollectables(GetCollectablesRequest request){
         if (request == null) {
-            return new GetCollectablesResponse();//TODO: pass parameters once class done
+            return new GetCollectablesResponse(false, "The GetCollectablesRequest object was null");
         }
+
+
 
         return null;
     }
