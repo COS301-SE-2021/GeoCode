be8bc45889b19053dd85dbf495c60f85cef15869
Liam <nitron.biohazard@gmail.com>
2021-05-29 16:27:09 +0200

    Added getters and setters for all variables.

--- backend/src/main/java/tech/geocodeapp/geocode/Collectable/Model/Collectable.java
+++ backend/src/main/java/tech/geocodeapp/geocode/Collectable/Model/Collectable.java
@@ -13,4 +13,28 @@ public class Collectable {
         this.name = name;
         this.rarity = rarity;
     }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Rarity getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(Rarity rarity) {
+        this.rarity = rarity;
+    }
 }
