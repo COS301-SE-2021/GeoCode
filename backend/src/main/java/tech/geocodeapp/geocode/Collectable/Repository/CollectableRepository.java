acf6f2fd47a1a8222969832b1582587d81ee05f3
Liam <nitron.biohazard@gmail.com>
2021-05-29 17:10:02 +0200

    Add missing tag and comment class

--- backend/src/main/java/tech/geocodeapp/geocode/Collectable/Repository/CollectableRepository.java
+++ backend/src/main/java/tech/geocodeapp/geocode/Collectable/Repository/CollectableRepository.java
@@ -1,7 +1,12 @@
 package tech.geocodeapp.geocode.Collectable.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.geocodeapp.geocode.Collectable.Model.Collectable;
 
+/*
+    This class implements the repository for the Collectable Subsystem
+ */
+@Repository
 public interface CollectableRepository extends JpaRepository<Collectable, String> {
 }
