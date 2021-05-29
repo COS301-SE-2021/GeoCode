4ce2eb3504335b548638516354b99428be1ed4e3
Liam <nitron.biohazard@gmail.com>
2021-05-29 16:24:27 +0200

    Added default and fully parameterized constructors.

--- backend/src/main/java/tech/geocodeapp/geocode/Collectable/Model/Collectable.java
+++ backend/src/main/java/tech/geocodeapp/geocode/Collectable/Model/Collectable.java
@@ -4,4 +4,13 @@ public class Collectable {
     private String image;
     private String name;
     private Rarity rarity;
+
+    public Collectable() {
+    }
+
+    public Collectable(String image, String name, Rarity rarity) {
+        this.image = image;
+        this.name = name;
+        this.rarity = rarity;
+    }
 }
