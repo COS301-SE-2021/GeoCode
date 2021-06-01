34242b6b8650587c308b284be1859c8a0ac2ab46
Liam <nitron.biohazard@gmail.com>
2021-06-02 00:31:16 +0200

    Added method to delete by CollectableSet and commented functions

--- backend/src/main/java/tech/geocodeapp/geocode/Collectable/Repository/CollectableTypeRepository.java
+++ backend/src/main/java/tech/geocodeapp/geocode/Collectable/Repository/CollectableTypeRepository.java
@@ -14,9 +14,24 @@ import java.util.UUID;
  */
 @Repository
 public interface CollectableTypeRepository extends JpaRepository<CollectableType, UUID> {
-    //get all CollectableTypes of a given rarity
+    /**
+     *  Gets all CollectableType objects of a given rarity
+     * @param rarity the Rarity enum value to search by
+     * @return A List of all found CollectableType objects
+     */
     List<CollectableType> getCollectableTypesByRarity(Rarity rarity);
 
-    //get all CollectableTypes of a given CollectableSet
+    /**
+     * Gets all CollectableTypes that are part of the provided CollectableSet
+     * @param set the CollectableSet to search by
+     * @return a List of CollectableType objects found
+     */
     List<CollectableType> getCollectableTypesBySet(CollectableSet set);
+
+    /**
+     * This method deletes all CollectableTypes of a provided CollectableSet
+     * @param set the CollectableSet to delete by
+     * @return returns a long containing the number of records deleted
+     */
+    long deleteCollectableTypesBySet(CollectableSet set);
 }
