ff06a9695ef8fd55e3c31e969130555f6154f344
Liam <nitron.biohazard@gmail.com>
2021-06-01 23:21:15 +0200

    Added findCollectableByType method

--- backend/src/main/java/tech/geocodeapp/geocode/Collectable/Repository/CollectableRepository.java
+++ backend/src/main/java/tech/geocodeapp/geocode/Collectable/Repository/CollectableRepository.java
@@ -1,9 +1,11 @@
 package tech.geocodeapp.geocode.Collectable.Repository;
 
 import io.swagger.model.Collectable;
+import io.swagger.model.CollectableType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -12,7 +14,7 @@ import java.util.UUID;
 @Repository
 public interface CollectableRepository extends JpaRepository<Collectable, UUID> {
     //SELECT
-
+    List<Collectable> findCollectableByType(CollectableType type);
     //UPDATE
 
     //DELETE
