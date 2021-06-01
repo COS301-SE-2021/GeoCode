3cd43569f553baa8ad400dac6833fadd8d5accbf
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-06-01 12:31:38 +0200

    Move swagger to new folder to match autogenerated package names

--- backend/src/main/java/io/swagger/RFC3339DateFormat.java
+++ backend/src/main/java/io/swagger/RFC3339DateFormat.java
@@ -1,4 +1,4 @@
-package tech.geocodeapp.geocode.swagger;
+package io.swagger;
 
 import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import com.fasterxml.jackson.databind.util.ISO8601Utils;
