57643778c72a87c4bdacfd5bc3df52dd828e987f
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-06-10 15:25:07 +0200

    Added google maps integration

--- frontend/src/app/geocode/geocode.page.ts
+++ frontend/src/app/geocode/geocode.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+declare let google;
 @Component({
   selector: 'app-geocode',
   templateUrl: './geocode.page.html',
   styleUrls: ['./geocode.page.scss'],
 })
-export class GeocodePage implements OnInit {
+export class GeocodePage implements AfterViewInit  {
+  @ViewChild('mapElement',{static:false}) mapElement;
 
+  map;
+
+  mapOptions = {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  };
   constructor() { }
 
-  ngOnInit() {
+  loadMap(){
+    this.map = new google.maps.Map(this.mapElement.nativeElement,this.mapOptions);
+  }
+
+  ngAfterViewInit(): void {
+     this.loadMap();
   }
 
 }
