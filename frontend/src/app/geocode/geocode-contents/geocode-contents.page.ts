477d91c6686fd090d8f4e1d0240b60f13dbf80a9
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-06-17 14:28:26 +0200

    Added map to display the geocode trying to find

--- frontend/src/app/geocode/geocode-contents/geocode-contents.page.ts
+++ frontend/src/app/geocode/geocode-contents/geocode-contents.page.ts
@@ -1,19 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+declare let google;
 @Component({
   selector: 'app-geocode-contents',
   templateUrl: './geocode-contents.page.html',
   styleUrls: ['./geocode-contents.page.scss'],
 })
-export class GeocodeContentsPage implements OnInit {
+export class GeocodeContentsPage implements AfterViewInit {
+  @ViewChild('mapElement',{static:false}) mapElement;
+  map;
+  geocode;
+  mapOptions;
 
-  constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
     this.route.queryParams.subscribe(params => {
-      console.log(params);
+          this.geocode= params.geocode;
+        });
+  }
+
+
+  // ngOnInit() {
+  //   this.route.queryParams.subscribe(params => {
+  //     console.log(params);
+  //   });
+  // }
+
+  //Create map and add mapmarkers of geocodes
+  loadMap(){
+
+    this.mapOptions = {
+      center: {lat: this.geocode.lat, lng: this.geocode.long},
+      zoom: 18,
+    };
+
+   this.map = new google.maps.Map(this.mapElement.nativeElement,this.mapOptions);
+
+   new google.maps.Marker({
+      position: {lat: this.geocode.lat, lng: this.geocode.long},
+      map: this.map,
+      title: '',
+
     });
+
+  }
+
+  ngAfterViewInit(): void {
+    this.loadMap();
   }
 
 }
