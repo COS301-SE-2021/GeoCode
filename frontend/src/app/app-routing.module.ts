38f9642b6519fe3fb1b4e89b898f586b6dfbbdd6
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-06-17 22:03:25 +0200

    Generated create GeoCode page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,12 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'geocode-create',
+    loadChildren: () => import('./geocode/geocode-create/geocode-create.module').then(m => m.GeocodeCreatePageModule)
   }
+
 
 
 ];
---
+++ frontend/src/app/geocode/geocode-create/geocode-create.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-geocode-create',
+  templateUrl: './geocode-create.page.html',
+  styleUrls: ['./geocode-create.page.scss'],
+})
+export class GeocodeCreatePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
