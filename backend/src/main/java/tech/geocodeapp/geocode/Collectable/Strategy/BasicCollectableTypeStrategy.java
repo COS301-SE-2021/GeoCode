c8eb77df1b91de8a42fab2b29eb2c5002598783d
Liam <nitron.biohazard@gmail.com>
2021-06-17 12:28:18 +0200

    Implemented getCollectableTypeFactory in BasicCollectableTypeStrategy

--- backend/src/main/java/tech/geocodeapp/geocode/Collectable/Strategy/BasicCollectableTypeStrategy.java
+++ backend/src/main/java/tech/geocodeapp/geocode/Collectable/Strategy/BasicCollectableTypeStrategy.java
@@ -1,4 +1,16 @@
 package tech.geocodeapp.geocode.Collectable.Strategy;
 
+import tech.geocodeapp.geocode.Collectable.Factory.AbstractCollectableTypeFactory;
+import tech.geocodeapp.geocode.Collectable.Factory.BasicCollectableTypeFactory;
+
 public class BasicCollectableTypeStrategy implements CollectableTypeStrategy {
+
+    /**
+     * Creates and returns the corresponding CollectableTypeFactory Class
+     * @return A new BasicCollectableTypeFactory to use
+     */
+    @Override
+    public AbstractCollectableTypeFactory getCollectableTypeFactory() {
+        return new BasicCollectableTypeFactory();
+    }
 }
