ea523383d96ce73e6b6883a06b1117e12f27a7a9
Caleb Johnstone <u19030119@tuks.co.za>
2021-06-18 22:28:05 +0200

    fixed Collectable API controller

--- backend/src/main/java/io/swagger/api/CollectableApiController.java
+++ backend/src/main/java/io/swagger/api/CollectableApiController.java
@@ -1,17 +1,6 @@
 package io.swagger.api;
 
-import io.swagger.model.Collectable;
-import io.swagger.model.CollectableSet;
-import io.swagger.model.CollectableType;
-import io.swagger.model.CreateCollectableRequest;
-import io.swagger.model.CreateCollectableSetRequest;
-import io.swagger.model.CreateCollectableTypeRequest;
-import io.swagger.model.GetCollectableByTypeRequest;
-import io.swagger.model.GetCollectableSetsResponse;
-import io.swagger.model.GetCollectableTypeByRarityRequest;
-import io.swagger.model.GetCollectableTypesBySetRequest;
-import io.swagger.model.GetCollectableTypesResponse;
-import io.swagger.model.GetCollectablesResponse;
+import io.swagger.model.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -24,6 +13,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +25,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import tech.geocodeapp.geocode.Collectable.Repository.CollectableRepository;
+import tech.geocodeapp.geocode.Collectable.Response.CreateCollectableResponse;
+import tech.geocodeapp.geocode.Collectable.Response.CreateCollectableSetResponse;
+import tech.geocodeapp.geocode.Collectable.Response.CreateCollectableTypeResponse;
+import tech.geocodeapp.geocode.Collectable.Service.CollectableServiceImpl;
 
 import javax.validation.constraints.*;
 import javax.validation.Valid;
@@ -43,7 +38,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-06-17T08:18:28.046Z[GMT]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-06-02T03:21:48.298Z[GMT]")
 @RestController
 public class CollectableApiController implements CollectableApi {
 
@@ -53,52 +48,44 @@ public class CollectableApiController implements CollectableApi {
 
     private final HttpServletRequest request;
 
+    @Autowired
+    private CollectableServiceImpl collectableService;
+
     @org.springframework.beans.factory.annotation.Autowired
     public CollectableApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
+
     }
 
-    public ResponseEntity<Collectable> createCollectable(@Parameter(in = ParameterIn.DEFAULT, description = "Request to create a new Collectable", required=true, schema=@Schema()) @Valid @RequestBody CreateCollectableRequest body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Collectable>(objectMapper.readValue("{\n  \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n  \"type\" : {\n    \"image\" : \"image\",\n    \"set\" : {\n      \"name\" : \"name\",\n      \"description\" : \"description\",\n      \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n    },\n    \"name\" : \"name\",\n    \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n    \"rarity\" : \"COMMON\"\n  }\n}", Collectable.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Collectable>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+    public ResponseEntity<CreateCollectableResponse> createCollectable(@Parameter(in = ParameterIn.DEFAULT, description = "Request to create a new Collectable", required=true, schema=@Schema()) @Valid @RequestBody CreateCollectableRequest body) {
+        CreateCollectableResponse response = collectableService.createCollectable(body);
 
-        return new ResponseEntity<Collectable>(HttpStatus.NOT_IMPLEMENTED);
+        if(response.isSuccess()){
+            return new ResponseEntity<CreateCollectableResponse>(response, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<CreateCollectableResponse>(response, HttpStatus.BAD_REQUEST);
+        }
     }
 
-    public ResponseEntity<CollectableSet> createCollectableSet(@Parameter(in = ParameterIn.DEFAULT, description = "Request to create a new Collectable Set", required=true, schema=@Schema()) @Valid @RequestBody CreateCollectableSetRequest body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<CollectableSet>(objectMapper.readValue("{\n  \"name\" : \"name\",\n  \"description\" : \"description\",\n  \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n}", CollectableSet.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<CollectableSet>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+    public ResponseEntity<CreateCollectableSetResponse> createCollectableSet(@Parameter(in = ParameterIn.DEFAULT, description = "Request to create a new Collectable Set", required=true, schema=@Schema()) @Valid @RequestBody CreateCollectableSetRequest body) {
+        CreateCollectableSetResponse response = collectableService.createCollectableSet(body);
 
-        return new ResponseEntity<CollectableSet>(HttpStatus.NOT_IMPLEMENTED);
+        if(response.isSuccess()){
+            return new ResponseEntity<CreateCollectableSetResponse>(response, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<CreateCollectableSetResponse>(response, HttpStatus.BAD_REQUEST);
+        }
     }
 
-    public ResponseEntity<CollectableType> createCollectableType(@Parameter(in = ParameterIn.DEFAULT, description = "Request to create a new Collectable Type", required=true, schema=@Schema()) @Valid @RequestBody CreateCollectableTypeRequest body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<CollectableType>(objectMapper.readValue("{\n  \"image\" : \"image\",\n  \"set\" : {\n    \"name\" : \"name\",\n    \"description\" : \"description\",\n    \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n  },\n  \"name\" : \"name\",\n  \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n  \"rarity\" : \"COMMON\"\n}", CollectableType.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<CollectableType>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+    public ResponseEntity<CreateCollectableTypeResponse> createCollectableType(@Parameter(in = ParameterIn.DEFAULT, description = "Request to create a new Collectable Type", required=true, schema=@Schema()) @Valid @RequestBody CreateCollectableTypeRequest body) {
+        CreateCollectableTypeResponse response = collectableService.createCollectableType(body);
 
-        return new ResponseEntity<CollectableType>(HttpStatus.NOT_IMPLEMENTED);
+        if(response.isSuccess()){
+            return new ResponseEntity<CreateCollectableTypeResponse>(response, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<CreateCollectableTypeResponse>(response, HttpStatus.BAD_REQUEST);
+        }
     }
 
     public ResponseEntity<GetCollectablesResponse> getCollectableByType(@Parameter(in = ParameterIn.DEFAULT, description = "Request Collectables by type", required=true, schema=@Schema()) @Valid @RequestBody GetCollectableByTypeRequest body) {
@@ -116,17 +103,13 @@ public class CollectableApiController implements CollectableApi {
     }
 
     public ResponseEntity<GetCollectableSetsResponse> getCollectableSets() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<GetCollectableSetsResponse>(objectMapper.readValue("{\n  \"collectableSets\" : [ {\n    \"name\" : \"name\",\n    \"description\" : \"description\",\n    \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n  }, {\n    \"name\" : \"name\",\n    \"description\" : \"description\",\n    \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n  } ]\n}", GetCollectableSetsResponse.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<GetCollectableSetsResponse>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+        GetCollectableSetsResponse response = collectableService.getCollectableSets();
 
-        return new ResponseEntity<GetCollectableSetsResponse>(HttpStatus.NOT_IMPLEMENTED);
+        if(!response.getCollectableSets().isEmpty()){
+            return new ResponseEntity<GetCollectableSetsResponse>(response, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<GetCollectableSetsResponse>(response, HttpStatus.NOT_FOUND);
+        }
     }
 
     public ResponseEntity<GetCollectableTypesResponse> getCollectableTypeByRarity(@Parameter(in = ParameterIn.DEFAULT, description = "Request Collectable Types by rarity", required=true, schema=@Schema()) @Valid @RequestBody GetCollectableTypeByRarityRequest body) {
@@ -158,31 +141,22 @@ public class CollectableApiController implements CollectableApi {
     }
 
     public ResponseEntity<GetCollectableTypesResponse> getCollectableTypes() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<GetCollectableTypesResponse>(objectMapper.readValue("{\n  \"collectableTypes\" : [ {\n    \"image\" : \"image\",\n    \"set\" : {\n      \"name\" : \"name\",\n      \"description\" : \"description\",\n      \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n    },\n    \"name\" : \"name\",\n    \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n    \"rarity\" : \"COMMON\"\n  }, {\n    \"image\" : \"image\",\n    \"set\" : {\n      \"name\" : \"name\",\n      \"description\" : \"description\",\n      \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n    },\n    \"name\" : \"name\",\n    \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n    \"rarity\" : \"COMMON\"\n  } ]\n}", GetCollectableTypesResponse.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<GetCollectableTypesResponse>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+        GetCollectableTypesResponse response = collectableService.getCollectableTypes();
 
-        return new ResponseEntity<GetCollectableTypesResponse>(HttpStatus.NOT_IMPLEMENTED);
+        if(!response.getCollectableTypes().isEmpty()){
+            return new ResponseEntity<GetCollectableTypesResponse>(response, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<GetCollectableTypesResponse>(response, HttpStatus.NOT_FOUND);
+        }
     }
 
     public ResponseEntity<GetCollectablesResponse> getCollectables() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<GetCollectablesResponse>(objectMapper.readValue("{\n  \"collectables\" : [ {\n    \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n    \"type\" : {\n      \"image\" : \"image\",\n      \"set\" : {\n        \"name\" : \"name\",\n        \"description\" : \"description\",\n        \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n      },\n      \"name\" : \"name\",\n      \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n      \"rarity\" : \"COMMON\"\n    }\n  }, {\n    \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n    \"type\" : {\n      \"image\" : \"image\",\n      \"set\" : {\n        \"name\" : \"name\",\n        \"description\" : \"description\",\n        \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\"\n      },\n      \"name\" : \"name\",\n      \"id\" : \"046b6c7f-0b8a-43b9-b35d-6489e6daee91\",\n      \"rarity\" : \"COMMON\"\n    }\n  } ]\n}", GetCollectablesResponse.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<GetCollectablesResponse>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+        GetCollectablesResponse response = collectableService.getCollectables();
+
+        if(!response.getCollectables().isEmpty()){
+            return new ResponseEntity<GetCollectablesResponse>(response, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<GetCollectablesResponse>(response, HttpStatus.NOT_FOUND);
         }
-
-        return new ResponseEntity<GetCollectablesResponse>(HttpStatus.NOT_IMPLEMENTED);
     }
-
 }
