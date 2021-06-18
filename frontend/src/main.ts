498c55596e7427421b29d030a884d5edb1d9bb85
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-06-18 09:36:43 +0200

    Added create geocode routing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,11 +7,10 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
- }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
-
 
 
 
