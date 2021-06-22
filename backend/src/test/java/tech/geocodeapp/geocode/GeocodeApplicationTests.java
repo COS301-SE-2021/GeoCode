7f7a954be670adbad5ab4653440eb696ee47b4c0
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2021-06-22 17:59:47 +0200

    removed unused imports

--- backend/src/test/java/tech/geocodeapp/geocode/GeocodeApplicationTests.java
+++ backend/src/test/java/tech/geocodeapp/geocode/GeocodeApplicationTests.java
@@ -1,8 +1,5 @@
 package tech.geocodeapp.geocode;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 //@SpringBootTest
 class GeocodeApplicationTests {
 
