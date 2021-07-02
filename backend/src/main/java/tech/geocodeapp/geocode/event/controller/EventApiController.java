e1922b10150555f7e2d5776ffb57d960274c4de4
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2021-07-03 00:07:01 +0200

    implemented packages and skeleton files

---
+++ backend/src/main/java/tech/geocodeapp/geocode/event/controller/EventApiController.java
@@ -0,0 +1,11 @@
+package tech.geocodeapp.geocode.event.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class EventApiController implements EventApi {
+
+
+}
