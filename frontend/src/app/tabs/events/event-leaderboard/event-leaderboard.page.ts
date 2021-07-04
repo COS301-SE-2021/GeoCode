fc93c90112a93772281ccfc27ace4440090ceb8b
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-07-04 22:26:25 +0200

    Basic LeaderBoard style created

--- frontend/src/app/tabs/events/event-leaderboard/event-leaderboard.page.ts
+++ frontend/src/app/tabs/events/event-leaderboard/event-leaderboard.page.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-leaderboard.page.scss'],
 })
 export class EventLeaderboardPage implements OnInit {
-
+points = [{username:'a',pos:1,points:100},{username:'a',pos:1,points:100},{username:'a',pos:1,points:100},{username:'a',pos:1,points:100},{username:'a',pos:1,points:100},
+  {username:'a',pos:1,points:100},{username:'a',pos:1,points:100},{username:'a',pos:1,points:100},{username:'a',pos:1,points:100},{username:'a',pos:1,points:100}];
   constructor() { }
 
   ngOnInit() {
