c9097e904c54cebf1d65884b6d1f6517b09b05f2
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-07-04 22:41:51 +0200

    Added map to timetrial

--- frontend/src/app/tabs/events/event-timetrial/event-timetrial.page.ts
+++ frontend/src/app/tabs/events/event-timetrial/event-timetrial.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 
+declare let google;
 @Component({
   selector: 'app-event-timetrial',
   templateUrl: './event-timetrial.page.html',
   styleUrls: ['./event-timetrial.page.scss'],
 })
-export class EventTimetrialPage implements OnInit {
-
+export class EventTimetrialPage implements AfterViewInit {
+  @ViewChild('mapElement',{static:false}) mapElement;
+  mapOptions;
+  map;
   constructor() { }
 
-  ngOnInit() {
+
+
+  ngAfterViewInit(): void {
+    this.loadMap();
+  }
+
+  //Create map and add mapmarkers of geocodes
+  loadMap(){
+    //Create map and center towards passed in geocode
+    this.mapOptions = {
+      center: {lat: -25.75625115327836, lng: 28.235629260918344},
+      zoom: 18,
+    };
+    //Create map
+    this.map = new google.maps.Map(this.mapElement.nativeElement,this.mapOptions);
   }
 
 }
