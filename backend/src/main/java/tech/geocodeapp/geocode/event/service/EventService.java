67732642267723cd109b5b6fd879d4b20fc71129
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2021-07-07 15:01:13 +0200

    service function skeleton

--- backend/src/main/java/tech/geocodeapp/geocode/event/service/EventService.java
+++ backend/src/main/java/tech/geocodeapp/geocode/event/service/EventService.java
@@ -1,11 +1,35 @@
 package tech.geocodeapp.geocode.event.service;
 
 
+import tech.geocodeapp.geocode.event.request.*;
+import tech.geocodeapp.geocode.event.response.*;
+
 /**
  * This is the main interface is for the Event subsystem,
  * it is used to call the relevant use cases to create, manipulate and delete Event.
  */
 public interface EventService {
 
+    ChangeAvailabilityResponse changeAvailability( ChangeAvailabilityRequest request );
+
+    GetAllEventsResponse getAllEvents();
+
+    CreateEventResponse createEvent( CreateEventRequest request );
+
+    GetEventsByLocationResponse getEventsByLocation( GetEventsByLocationRequest request );
+
+    CreatePointResponse createPoint( CreatePointRequest request );
+
+    CreateTimeTrialResponse createTimeTrial( CreateTimeTrialRequest request );
+
+    GetPointsByUserResponse getPointsByUser( GetPointsByUserRequest request );
+
+    GetPointsByLeaderBoardResponse getPointsByLeaderboard( GetPointsByLeaderBoardRequest request );
+
+    GetPointsResponse getPoints();
+
+    GetLeaderBoardByTimeTrialResponse getLeaderBoardByTimeTrial( GetLeaderBoardByTimeTrialRequest request );
+
+    CreateLeaderboardResponse createLeaderBoard( CreateLeaderboardRequest request );
 
 }
--- backend/src/main/java/tech/geocodeapp/geocode/event/service/EventServiceImpl.java
+++ backend/src/main/java/tech/geocodeapp/geocode/event/service/EventServiceImpl.java
@@ -3,6 +3,8 @@ package tech.geocodeapp.geocode.event.service;
 import org.springframework.stereotype.Service;
 
 import org.springframework.validation.annotation.Validated;
+import tech.geocodeapp.geocode.event.request.*;
+import tech.geocodeapp.geocode.event.response.*;
 
 
 /**
@@ -12,5 +14,71 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class EventServiceImpl implements EventService {
 
+
+    @Override
+    public ChangeAvailabilityResponse changeAvailability( ChangeAvailabilityRequest request ) {
+
+        return null;
+    }
+
+    @Override
+    public GetAllEventsResponse getAllEvents() {
+
+        return null;
+    }
+
+    @Override
+    public CreateEventResponse createEvent( CreateEventRequest request ) {
+
+        return null;
+    }
+
+    @Override
+    public GetEventsByLocationResponse getEventsByLocation( GetEventsByLocationRequest request ) {
+
+        return null;
+    }
+
+    @Override
+    public CreatePointResponse createPoint( CreatePointRequest request ) {
+
+        return null;
+    }
+
+    @Override
+    public CreateTimeTrialResponse createTimeTrial( CreateTimeTrialRequest request ) {
+
+        return null;
+    }
+
+    @Override
+    public GetPointsByUserResponse getPointsByUser( GetPointsByUserRequest request ) {
+
+        return null;
+    }
+
+    @Override
+    public GetPointsByLeaderBoardResponse getPointsByLeaderboard( GetPointsByLeaderBoardRequest request ) {
+
+        return null;
+    }
+
+    @Override
+    public GetPointsResponse getPoints() {
+
+        return null;
+    }
+
+    @Override
+    public GetLeaderBoardByTimeTrialResponse getLeaderBoardByTimeTrial( GetLeaderBoardByTimeTrialRequest request ) {
+
+        return null;
+    }
+
+    @Override
+    public CreateLeaderboardResponse createLeaderBoard( CreateLeaderboardRequest request ) {
+
+        return null;
+    }
 
 }
