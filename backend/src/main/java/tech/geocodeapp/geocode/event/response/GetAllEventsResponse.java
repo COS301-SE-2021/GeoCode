c1ddc00e1a906f772c338743d4e352666d579d0f
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2021-07-09 15:03:05 +0200

    added comments and created overloaded constructor

--- backend/src/main/java/tech/geocodeapp/geocode/event/response/GetAllEventsResponse.java
+++ backend/src/main/java/tech/geocodeapp/geocode/event/response/GetAllEventsResponse.java
@@ -1,78 +1,123 @@
 package tech.geocodeapp.geocode.event.response;
 
-import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.validation.annotation.Validated;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
 import tech.geocodeapp.geocode.event.model.Event;
 
-import javax.validation.Valid;
-import javax.validation.constraints.*;
+import java.util.Objects;
+import java.util.List;
 
 /**
- * GetAllEventsResponse
+ * GetAllEventsResponse object returns all the Event objects in the system
  */
 @Validated
 public class GetAllEventsResponse {
 
+    /**
+     * All the Event's stored inside of the EventsRepository
+     */
     @JsonProperty( "events" )
-    @Valid
-    private List< Event > events = new ArrayList<>();
+    @NotNull( message = "GetAllEventsResponse events cannot be null." )
+    private List< Event > events;
 
-    public GetAllEventsResponse events( List< Event > events ) {
+    /**
+     * Overloaded Constructor
+     *
+     * @param events the list of events to set the events attribute to
+     */
+    public GetAllEventsResponse( @Valid List< Event > events ) {
+
+        this.events = events;
+    }
+
+    /**
+     * Sets the events attribute to the specified value
+     *
+     * @param events the value the attribute should be set to
+     *
+     * @return the request after the events has been changed
+     */
+    public GetAllEventsResponse events( @Valid List< Event > events ) {
 
         this.events = events;
         return this;
     }
 
-    public GetAllEventsResponse addEventsItem( Event eventsItem ) {
+    /**
+     * Sets a single event inside of the events attribute to the specified value
+     *
+     * @param eventsItem the value the attribute should be set to
+     *
+     * @return the stored events attribute
+     */
+    public GetAllEventsResponse addEventsItem( @Valid Event eventsItem ) {
 
         this.events.add( eventsItem );
         return this;
     }
 
     /**
-     * Get events
+     * Gets the saved events attribute
      *
-     * @return events
-     **/
-    @Schema( required = true, description = "" )
-    @NotNull
+     * @return the stored geocodes attribute
+     */
     @Valid
     public List< Event > getEvents() {
 
         return events;
     }
 
-    public void setEvents( List< Event > events ) {
+    /**
+     * Sets the events attribute to the specified value
+     *
+     * @param events the value the attribute should be set to
+     */
+    public void setEvents( @Valid List< Event > events ) {
 
         this.events = events;
     }
 
-
+    /**
+     * Determines if the specified object is the same as the current object
+     *
+     * @param obj the object we want to compare with the specific attributes of this class
+     *
+     * @return if the object is the same or not
+     */
     @Override
-    public boolean equals( java.lang.Object o ) {
+    public boolean equals( java.lang.Object obj ) {
 
-        if ( this == o ) {
+        if ( this == obj ) {
 
             return true;
         }
-        if ( o == null || getClass() != o.getClass() ) {
+        if ( obj == null || getClass() != obj.getClass() ) {
 
             return false;
         }
-        GetAllEventsResponse getAllEventsResponse = ( GetAllEventsResponse ) o;
-        return Objects.equals( this.events, getAllEventsResponse.events );
+
+        return Objects.equals( this.events, ( ( GetAllEventsResponse ) obj ).events );
     }
 
+    /**
+     * Creates a hash code from the attributes in the class
+     *
+     * @return the created has code
+     */
     @Override
     public int hashCode() {
 
         return Objects.hash( events );
     }
 
+    /**
+     * Creates a string from all the attributes in the class
+     *
+     * @return the created string
+     */
     @Override
     public String toString() {
 
