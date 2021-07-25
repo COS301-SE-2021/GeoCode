6894f1ff32852835e7d6e9300269b4cf9a322788
Liam <nitron.biohazard@gmail.com>
2021-07-25 18:05:28 +0200

    Added variables to UpdatePointResponse

--- backend/src/main/java/tech/geocodeapp/geocode/leaderboard/response/UpdatePointResponse.java
+++ backend/src/main/java/tech/geocodeapp/geocode/leaderboard/response/UpdatePointResponse.java
@@ -1,4 +1,11 @@
 package tech.geocodeapp.geocode.leaderboard.response;
 
+import tech.geocodeapp.geocode.leaderboard.model.Point;
+
 public class UpdatePointResponse {
+
+    private Point point;
+    private String message;
+    private boolean success;
+
 }
