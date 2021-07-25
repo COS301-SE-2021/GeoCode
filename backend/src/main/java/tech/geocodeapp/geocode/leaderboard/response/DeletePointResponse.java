79bebc5c70036949ad4cba694fcb686422f68b08
Liam <nitron.biohazard@gmail.com>
2021-07-25 17:59:07 +0200

    Added getters and setters to DeletePointResponse

--- backend/src/main/java/tech/geocodeapp/geocode/leaderboard/response/DeletePointResponse.java
+++ backend/src/main/java/tech/geocodeapp/geocode/leaderboard/response/DeletePointResponse.java
@@ -12,4 +12,20 @@ public class DeletePointResponse {
         this.message = message;
         this.success = success;
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
