88b08ab90831aaaf3bf975084016eeedfcd68e59
Liam <nitron.biohazard@gmail.com>
2021-07-25 18:01:29 +0200

    Added variables to UpdatePointRequest

--- backend/src/main/java/tech/geocodeapp/geocode/leaderboard/request/UpdatePointRequest.java
+++ backend/src/main/java/tech/geocodeapp/geocode/leaderboard/request/UpdatePointRequest.java
@@ -1,4 +1,11 @@
 package tech.geocodeapp.geocode.leaderboard.request;
 
+import java.util.UUID;
+
 public class UpdatePointRequest {
+
+    private UUID pointId;
+    private Integer amount;
+    private UUID userId;
+    private UUID leaderboardId;
 }
