ef9002fe0bbce342dab04eae28396c3c6791e055
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-07-25 15:53:07 +0200

    Added map loader function

--- frontend/src/app/tabs/events/events.page.ts
+++ frontend/src/app/tabs/events/events.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {GoogleMapsLoader} from '../../services/GoogleMapsLoader';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.page.html',
   styleUrls: ['./events.page.scss'],
 })
-export class EventsPage implements OnInit {
+export class EventsPage implements AfterViewInit {
+  @ViewChild('mapElement',{static:false}) mapElement;
+  googleMaps;
+  mapOptions;
+  map;
+  mapMarker;
+  markers= [];
+  isHidden=false;
+  height='92%';
+  constructor(private mapsLoader: GoogleMapsLoader) { }
 
-  constructor() { }
+  //Create map and add mapmarkers of geocodes
+  loadMap(){
+    this.markers= [];
+    this.mapOptions = {
+      center: {lat: -25.75625115327836, lng: 28.235629260918344},
+      zoom: 15,
+    };
+    this.map = new this.googleMaps.Map(this.mapElement.nativeElement,this.mapOptions);
 
-  ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    this.mapsLoader.load().then(handle => {
+      this.googleMaps = handle;
+      this.loadMap();
+    }).catch();
   }
 
 }
