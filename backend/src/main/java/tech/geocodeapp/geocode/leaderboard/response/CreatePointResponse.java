33e98b77cb46f6aeeb89bce55193bba4ad78547a
Liam <nitron.biohazard@gmail.com>
2021-07-25 17:53:32 +0200

    Added required variables, getters, setters and constructors to CreatePointResponse

--- backend/src/main/java/tech/geocodeapp/geocode/leaderboard/response/CreatePointResponse.java
+++ backend/src/main/java/tech/geocodeapp/geocode/leaderboard/response/CreatePointResponse.java
@@ -1,4 +1,43 @@
 package tech.geocodeapp.geocode.leaderboard.response;
 
+import tech.geocodeapp.geocode.leaderboard.model.Point;
+
 public class CreatePointResponse {
+
+    private Point point;
+    private String message;
+    private boolean success;
+
+    public CreatePointResponse() {
+    }
+
+    public CreatePointResponse(Point point, String message, boolean success) {
+        this.point = point;
+        this.message = message;
+        this.success = success;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
