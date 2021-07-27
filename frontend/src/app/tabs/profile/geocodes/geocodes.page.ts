65a71ea4d9935c7cf43ddc069166c43f89e24655
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-07-27 19:56:37 +0200

    Add basic geocodes page to profile

--- frontend/src/app/tabs/profile/geocodes/geocodes.page.ts
+++ frontend/src/app/tabs/profile/geocodes/geocodes.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Collectable, GeoCode} from '../../../services/geocode-api';
+import {GoogleMapsLoader} from '../../../services/GoogleMapsLoader';
 
 @Component({
   selector: 'app-geocodes',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeocodesPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('mapElement',{static:false}) mapElement;
+  googleMaps;
+  map;
 
-  ngOnInit() {
+  created: GeoCode[];
+  found: GeoCode[];
+
+  constructor(
+    private mapsLoader: GoogleMapsLoader
+  ) {
+    this.created = [null];
   }
 
+  async ngOnInit() {
+    this.googleMaps = await this.mapsLoader.load();
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.loadMap(position.coords.latitude, position.coords.longitude);
+    }, (positionError) => {
+      this.loadMap(0, 0);
+    });
+  }
+
+  async loadMap(centreLat, centreLong) {
+    const mapOptions = {
+      center: {lat: centreLat, lng: centreLong},
+      zoom: 5,
+    };
+    this.map = new this.googleMaps.Map(this.mapElement.nativeElement, mapOptions);
+    await this.loadFound();
+  }
+
+  async segmentChanged(event) {
+    if (event.target.value === 'found') {
+      await this.loadFound();
+    } else if (event.target.value === 'created') {
+      await this.loadCreated();
+    }
+  }
+
+  async loadFound() {
+    console.log('Showing found geocodes');
+  }
+
+  async loadCreated() {
+    console.log('Showing created geocodes');
+  }
 }
