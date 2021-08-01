9b4e344bb645581157e5b0dadc08209c7de5abca
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-08-01 22:54:41 +0200

    Added map to mission page

--- frontend/src/app/tabs/mission/mission.page.ts
+++ frontend/src/app/tabs/mission/mission.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {GoogleMapsLoader} from '../../services/GoogleMapsLoader';
 
 @Component({
   selector: 'app-mission',
   templateUrl: './mission.page.html',
   styleUrls: ['./mission.page.scss'],
 })
-export class MissionPage implements OnInit {
+export class MissionPage implements AfterViewInit {
+  @ViewChild('mapElement',{static:false}) mapElement;
+  googleMaps;
+  mapOptions;
+  map;
+  mapMarker;
+  markers= [];
+  selected=[];
+  events=[];
+  isHidden=false;
+  height='60%';
+  constructor(private mapsLoader: GoogleMapsLoader) { }
 
-  constructor() { }
+  //Create map and add mapmarkers of geocodes
+  loadMap(){
+    this.markers= [];
+    this.mapOptions = {
+      center: {lat: -25.75625115327836, lng: 28.235629260918344},
+      zoom: 15,
+    };
+    this.map = new this.googleMaps.Map(this.mapElement.nativeElement,this.mapOptions);
 
-  ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    this.mapsLoader.load().then(handle => {
+      this.googleMaps = handle;
+      this.loadMap();
+    }).catch();
   }
 
 }
