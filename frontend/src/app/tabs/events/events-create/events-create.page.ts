b1e2ebc987fa7fe4687d7bf96b30107c0e2871f3
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-08-01 17:48:49 +0200

    Generated create events page

---
+++ frontend/src/app/tabs/events/events-create/events-create.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-create',
+  templateUrl: './events-create.page.html',
+  styleUrls: ['./events-create.page.scss'],
+})
+export class EventsCreatePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
