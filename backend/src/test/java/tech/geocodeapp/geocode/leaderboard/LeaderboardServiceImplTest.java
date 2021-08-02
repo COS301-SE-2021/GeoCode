c73e94ffa4f9716914dfe86113c798fe1c27260a
Liam <nitron.biohazard@gmail.com>
2021-08-02 16:26:21 +0200

    Created test for when updatePoint receives a request with the only an invalid userId to update the point with

--- backend/src/test/java/tech/geocodeapp/geocode/leaderboard/LeaderboardServiceImplTest.java
+++ backend/src/test/java/tech/geocodeapp/geocode/leaderboard/LeaderboardServiceImplTest.java
@@ -410,6 +410,7 @@ public class LeaderboardServiceImplTest {
             RegisterNewUserResponse userResponse = userService.registerNewUser(userRequest);
             CreatePointRequest createPointRequest = new CreatePointRequest(1, userId, leaderboardResponse.getLeaderboard().getId());
             PointResponse createPointResponse = leaderboardService.createPoint(createPointRequest);
+
             CreateLeaderboardRequest updatedLeaderboardRequest = new CreateLeaderboardRequest("updated leaderboard");
             CreateLeaderboardResponse updatedLeaderboardResponse = leaderboardService.createLeaderboard(updatedLeaderboardRequest);
             UpdatePointRequest request = new UpdatePointRequest(createPointResponse.getPoint().getId(), null, null, updatedLeaderboardResponse.getLeaderboard().getId());
@@ -419,6 +420,29 @@ public class LeaderboardServiceImplTest {
             Assertions.assertEquals("Updated point successfully", response.getMessage());
             Assertions.assertNotNull(response.getPoint());
             Assertions.assertEquals(updatedLeaderboardResponse.getLeaderboard(), response.getPoint().getLeaderBoard());
+        } catch (NullRequestParameterException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void updatePointTestInvalidUserId() {
+        CreateLeaderboardRequest leaderboardRequest = new CreateLeaderboardRequest("test");
+        try {
+            CreateLeaderboardResponse leaderboardResponse = leaderboardService.createLeaderboard(leaderboardRequest);
+            UUID userId = UUID.randomUUID();
+            RegisterNewUserRequest userRequest = new RegisterNewUserRequest(userId, "test user");
+            RegisterNewUserResponse userResponse = userService.registerNewUser(userRequest);
+            CreatePointRequest createPointRequest = new CreatePointRequest(1, userId, leaderboardResponse.getLeaderboard().getId());
+            PointResponse pointResponse = leaderboardService.createPoint(createPointRequest);
+
+            UpdatePointRequest request = new UpdatePointRequest(pointResponse.getPoint().getId(), null, UUID.randomUUID(), null);
+            PointResponse response = leaderboardService.updatePoint(request);
+
+            Assertions.assertFalse(response.isSuccess());
+            Assertions.assertEquals("Invalid UserId to update to was provided", response.getMessage());
+            Assertions.assertNull(response.getPoint());
         } catch (NullRequestParameterException e) {
             e.printStackTrace();
         }
