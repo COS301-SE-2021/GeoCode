d04005d3aea68a115c718e22e793c9f831a55a9d
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-08-03 16:18:46 +0200

    Add user missions list linking to mission example page

--- frontend/src/app/tabs/profile/missions/missions.page.ts
+++ frontend/src/app/tabs/profile/missions/missions.page.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserMissionsPage implements OnInit {
 
+  missions = [{
+    name: 'Mission Name',
+    id: 'random',
+    progress: 0.42
+  },{
+    name: 'Mission Name',
+    id: 'random',
+    progress: 0.42
+  }];
+
   constructor() { }
 
   ngOnInit() {
