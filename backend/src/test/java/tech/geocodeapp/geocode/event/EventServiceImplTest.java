d8416012be772b3edf227ccd54479296fdfe2a9e
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2021-08-05 08:44:38 +0200

    implemented tests for getEvent

--- backend/src/test/java/tech/geocodeapp/geocode/event/EventServiceImplTest.java
+++ backend/src/test/java/tech/geocodeapp/geocode/event/EventServiceImplTest.java
@@ -7,12 +7,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import tech.geocodeapp.geocode.event.exceptions.InvalidRequestException;
 import tech.geocodeapp.geocode.event.exceptions.RepoException;
+import tech.geocodeapp.geocode.event.model.Event;
 import tech.geocodeapp.geocode.event.request.CreateEventRequest;
+import tech.geocodeapp.geocode.event.request.GetEventRequest;
 import tech.geocodeapp.geocode.event.response.CreateEventResponse;
+import tech.geocodeapp.geocode.event.response.GetEventResponse;
 import tech.geocodeapp.geocode.event.service.EventService;
 import tech.geocodeapp.geocode.event.service.EventServiceImpl;
+import tech.geocodeapp.geocode.geocode.model.Difficulty;
 import tech.geocodeapp.geocode.geocode.model.GeoCode;
 import tech.geocodeapp.geocode.geocode.model.GeoPoint;
+import tech.geocodeapp.geocode.geocode.request.CreateGeoCodeRequest;
 import tech.geocodeapp.geocode.leaderboard.LeaderboardMockRepository;
 import tech.geocodeapp.geocode.leaderboard.PointMockRepository;
 import tech.geocodeapp.geocode.leaderboard.UserMockRepository;
@@ -68,6 +73,11 @@ class EventServiceImplTest {
      * The expected exception message for if the given request has invalid attributes
      */
     String reqEmptyError = "The given request is empty.";
+
+    /**
+     * This is used to have a static known UUID
+     */
+    UUID eventID = UUID.fromString( "db91e6ee-f5b6-11eb-9a03-0242ac130003" );
 
     /**
      * Create the EventServiceImpl with the relevant repositories.
@@ -156,7 +166,7 @@ class EventServiceImplTest {
     }
 
     /**
-     * Using valid data does the createGeoCode use case test
+     * Using valid data does the createEvent use case test
      * complete successfully
      */
     @Test
@@ -195,5 +205,120 @@ class EventServiceImplTest {
             /* An error occurred, print the stack to identify */
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Check how the use case handles the request being null
+     */
+    @Test
+    @Order( 5 )
+    @DisplayName( "Null repository handling - getEvent" )
+    void getEventCodeNullRequestTest() {
+
+        /* Null request check */
+        assertThatThrownBy( () -> eventService.getEvent( null ) )
+                .isInstanceOf( InvalidRequestException.class )
+                .hasMessageContaining( reqEmptyError );
+    }
+
+    /**
+     * Check how the use case handles an invalid request
+     */
+    @Test
+    @Order( 6 )
+    @DisplayName( "Invalid repository attribute handling - getEvent" )
+    void getEventInvalidRequestTest() {
+
+        /*
+         *  Create a request object
+         * and assign values to it
+         */
+        GetEventRequest request = new GetEventRequest();
+        request.setEventID( null );
+
+        /* Null parameter request check */
+        assertThatThrownBy( () -> eventService.getEvent( request ) )
+                .isInstanceOf( InvalidRequestException.class )
+                .hasMessageContaining( reqParamError );
+    }
+
+    /**
+     * Using valid data does the getEvent use case test
+     * complete successfully
+     */
+    @Test
+    @Order( 7 )
+    @DisplayName( "Valid request - getEvent" )
+    void getEventTest() {
+
+        try {
+
+            /* Insert different random Events into the repository */
+            populate( 3 );
+
+            /* Populate with a known Event to find*/
+            var event = new Event( eventID, "Test", "Test description", null,
+                                   null, LocalDate.parse("2020-01-08"),
+                                   LocalDate.parse("2020-01-08"), null);
+            repo.save( event );
+
+            /*
+             * Create a request object
+             * and assign values to it
+             */
+            GetEventRequest request = new GetEventRequest();
+            request.setEventID( eventID );
+
+            GetEventResponse response = eventService.getEvent( request );
+
+            /*
+             * Check if the Event was created correctly found
+             */
+            Assertions.assertEquals( event, response.getFoundEvent() );
+            Assertions.assertEquals( event.getDescription(), response.getFoundEvent().getDescription() );
+
+            /* Check if the name is not the same as one of the random populated names */
+            Assertions.assertNotEquals( "Super Sport", response.getFoundEvent().getName() );
+        } catch ( InvalidRequestException e ) {
+
+            /* An error occurred, print the stack to identify */
+            e.printStackTrace();
+        }
+    }
+
+
+    ////////////////Helper functions////////////////
+
+    /**
+     * This function creates numerous Events to be used for testing.
+     *
+     * NOTE: the create function will need to be working with tests passing for this
+     *       helper function to be used
+     */
+    private void populate( int size ) {
+
+        try {
+
+
+            CreateEventRequest request = new CreateEventRequest();
+            request.setDescription( "Try get as many as possible" );
+            request.setLocation( new GeoPoint( 10.2587, 40.336981 ) );
+            request.setName( "Super Sport" );
+            request.setBeginDate( LocalDate.parse("2020-01-08") );
+            request.setEndDate(  LocalDate.parse("2020-05-21") );
+            List< GeoCode > geoCodesToFind = new ArrayList<>();
+            geoCodesToFind.add( new GeoCode() );
+            geoCodesToFind.add( new GeoCode() );
+            geoCodesToFind.add( new GeoCode() );
+            request.setGeoCodesToFind( geoCodesToFind );
+
+            eventService.createEvent( request );
+
+        } catch ( InvalidRequestException e ) {
+
+            /* An error occurred, print the stack to identify */
+            e.printStackTrace();
+        }
+
     }
 }
