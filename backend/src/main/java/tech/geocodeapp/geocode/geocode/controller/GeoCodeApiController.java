6945e9c58864e6280096024460cccc5fe222583f
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2021-08-06 17:46:29 +0200

    updated https status success status

--- backend/src/main/java/tech/geocodeapp/geocode/geocode/controller/GeoCodeApiController.java
+++ backend/src/main/java/tech/geocodeapp/geocode/geocode/controller/GeoCodeApiController.java
@@ -26,11 +26,14 @@ public class GeoCodeApiController implements GeoCodeApi {
         this.geoCodeService = geoCodeService;
     }
 
-    public ResponseEntity< CreateGeoCodeResponse > createGeoCode( @Parameter( in = ParameterIn.DEFAULT, description = "Request to create a new GeoCode", required = true, schema = @Schema() ) @Valid @RequestBody CreateGeoCodeRequest body ) throws InvalidRequestException {
+    public ResponseEntity< CreateGeoCodeResponse > createGeoCode( @Parameter( in = ParameterIn.DEFAULT,
+                                                                  description = "Request to create a new GeoCode",
+                                                                  required = true, schema = @Schema() )
+                                                                  @Valid @RequestBody CreateGeoCodeRequest body ) throws InvalidRequestException {
 
         CreateGeoCodeResponse response = geoCodeService.createGeoCode( body );
 
-        if ( ( response.isIsSuccess() != null ) && ( Boolean.TRUE.equals( response.isIsSuccess() ) ) ) {
+        if ( ( response != null ) && ( response.isIsSuccess() != null ) ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
@@ -39,11 +42,14 @@ public class GeoCodeApiController implements GeoCodeApi {
         }
     }
 
-    public ResponseEntity< GetGeoCodeByLocationResponse > getGeoCodeByLocation( @Parameter( in = ParameterIn.DEFAULT, description = "Request to get a GeoCode at or near the given location", required = true, schema = @Schema() ) @Valid @RequestBody GetGeoCodeByLocationRequest body ) throws InvalidRequestException {
+    public ResponseEntity< GetGeoCodeByLocationResponse > getGeoCodeByLocation( @Parameter( in = ParameterIn.DEFAULT,
+                                                                                description = "Request to get a GeoCode at or near the given location",
+                                                                                required = true, schema = @Schema() )
+                                                                                @Valid @RequestBody GetGeoCodeByLocationRequest body ) throws InvalidRequestException {
 
         GetGeoCodeByLocationResponse response = geoCodeService.getGeoCodesByLocation( body );
 
-        if ( response.getId() != null ) {
+        if (  ( response != null ) && ( response.getId() != null ) ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
@@ -52,11 +58,14 @@ public class GeoCodeApiController implements GeoCodeApi {
         }
     }
 
-    public ResponseEntity< GetGeoCodeByQRCodeResponse > getGeoCodeByQRCode( @Parameter( in = ParameterIn.DEFAULT, description = "Request to get a GeoCode's associated with the given QR Code", required = true, schema = @Schema() ) @Valid @RequestBody GetGeoCodeByQRCodeRequest body ) throws InvalidRequestException {
+    public ResponseEntity< GetGeoCodeByQRCodeResponse > getGeoCodeByQRCode( @Parameter( in = ParameterIn.DEFAULT,
+                                                                            description = "Request to get a GeoCode's associated with the given QR Code",
+                                                                            required = true, schema = @Schema() )
+                                                                            @Valid @RequestBody GetGeoCodeByQRCodeRequest body ) throws InvalidRequestException {
 
         GetGeoCodeByQRCodeResponse response = geoCodeService.getGeoCodeByQRCode( body );
 
-        if ( response.getId() != null ) {
+        if (  ( response != null ) && ( response.getId() != null ) ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
@@ -65,11 +74,14 @@ public class GeoCodeApiController implements GeoCodeApi {
         }
     }
 
-    public ResponseEntity< GetCollectablesResponse > getGeoCodeCollectables( @Parameter( in = ParameterIn.DEFAULT, description = "Request to get a GeoCode's Collectables", required = true, schema = @Schema() ) @Valid @RequestBody GetCollectablesRequest body ) throws InvalidRequestException {
+    public ResponseEntity< GetCollectablesResponse > getGeoCodeCollectables( @Parameter( in = ParameterIn.DEFAULT,
+                                                                            description = "Request to get a GeoCode's Collectables",
+                                                                            required = true, schema = @Schema() )
+                                                                             @Valid @RequestBody GetCollectablesRequest body ) throws InvalidRequestException {
 
         GetCollectablesResponse response = geoCodeService.getCollectables( body );
 
-        if ( ( response.getCollectables() != null ) && ( !response.getCollectables().isEmpty() ) ) {
+        if (  ( response != null ) && ( response.getCollectables() != null ) && ( !response.getCollectables().isEmpty() ) ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
@@ -78,50 +90,59 @@ public class GeoCodeApiController implements GeoCodeApi {
         }
     }
 
-    public ResponseEntity< GetGeoCodesResponse > getGeoCodes() throws RepoException {
+    public ResponseEntity< GetGeoCodesResponse > getGeoCodes() {
 
         GetGeoCodesResponse response = geoCodeService.getAllGeoCodes();
 
-        if ( ( response.getGeocodes() != null ) && ( !response.getGeocodes().isEmpty() ) ) {
+        if (  ( response != null ) ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
 
-            return new ResponseEntity<>( response, HttpStatus.BAD_REQUEST );
+            return new ResponseEntity<>( null, HttpStatus.BAD_REQUEST );
         }
     }
 
-    public ResponseEntity< GetGeoCodesByDifficultyResponse > getGeoCodesByDifficulty( @Parameter( in = ParameterIn.DEFAULT, description = "Request to get all the GeoCodes by the specified difficulty", required = true, schema = @Schema() ) @Valid @RequestBody GetGeoCodesByDifficultyRequest body ) throws InvalidRequestException {
+    public ResponseEntity< GetGeoCodesByDifficultyResponse > getGeoCodesByDifficulty( @Parameter( in = ParameterIn.DEFAULT,
+                                                                                      description = "Request to get all the GeoCodes by the specified difficulty",
+                                                                                      required = true, schema = @Schema() )
+                                                                                      @Valid @RequestBody GetGeoCodesByDifficultyRequest body ) throws InvalidRequestException {
 
         GetGeoCodesByDifficultyResponse response = geoCodeService.getGeoCodesByDifficulty( body );
 
-        if ( ( response.getGeocodes() != null ) && ( !response.getGeocodes().isEmpty() ) ) {
+        if ( response != null ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
 
-            return new ResponseEntity<>( response, HttpStatus.BAD_REQUEST );
+            return new ResponseEntity<>( null, HttpStatus.BAD_REQUEST );
         }
     }
 
-    public ResponseEntity< GetHintsResponse > getHints( @Parameter( in = ParameterIn.DEFAULT, description = "Request to get the hints from the specified GeoCode", required = true, schema = @Schema() ) @Valid @RequestBody GetHintsRequest body ) throws InvalidRequestException, RepoException {
+    public ResponseEntity< GetHintsResponse > getHints( @Parameter( in = ParameterIn.DEFAULT,
+                                                        description = "Request to get the hints from the specified GeoCode",
+                                                        required = true, schema = @Schema() )
+                                                        @Valid @RequestBody GetHintsRequest body ) throws InvalidRequestException, RepoException {
 
         GetHintsResponse response = geoCodeService.getHints( body );
 
-        if ( ( response.getHints() != null ) && ( !response.getHints().isEmpty() ) ) {
+        if ( response != null ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
 
-            return new ResponseEntity<>( response, HttpStatus.BAD_REQUEST );
+            return new ResponseEntity<>( null, HttpStatus.BAD_REQUEST );
         }
     }
 
-    public ResponseEntity< SwapCollectablesResponse > swapCollectables( @Parameter( in = ParameterIn.DEFAULT, description = "Request to swap a GeoCode's Collectables", required = true, schema = @Schema() ) @Valid @RequestBody SwapCollectablesRequest body ) throws InvalidRequestException {
+    public ResponseEntity< SwapCollectablesResponse > swapCollectables( @Parameter( in = ParameterIn.DEFAULT,
+                                                                        description = "Request to swap a GeoCode's Collectables",
+                                                                        required = true, schema = @Schema() )
+                                                                        @Valid @RequestBody SwapCollectablesRequest body ) throws InvalidRequestException {
 
         SwapCollectablesResponse response = geoCodeService.swapCollectables( body );
 
-        if ( response.isIsSuccess() != null ) {
+        if ( ( response != null ) && ( response.isIsSuccess() != null ) ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
@@ -130,11 +151,14 @@ public class GeoCodeApiController implements GeoCodeApi {
         }
     }
 
-    public ResponseEntity< UpdateAvailabilityResponse > updateAvailability( @Parameter( in = ParameterIn.DEFAULT, description = "Request to update a GeoCode's availability", required = true, schema = @Schema() ) @Valid @RequestBody UpdateAvailabilityRequest body ) throws InvalidRequestException {
+    public ResponseEntity< UpdateAvailabilityResponse > updateAvailability( @Parameter( in = ParameterIn.DEFAULT,
+                                                                            description = "Request to update a GeoCode's availability",
+                                                                            required = true, schema = @Schema() )
+                                                                            @Valid @RequestBody UpdateAvailabilityRequest body ) throws InvalidRequestException {
 
         UpdateAvailabilityResponse response = geoCodeService.updateAvailability( body );
 
-        if ( ( response.isIsSuccess() != null ) && ( Boolean.TRUE.equals( response.isIsSuccess() ) ) ) {
+        if (  ( response != null ) && ( response.isIsSuccess() != null ) ) {
 
             return new ResponseEntity<>( response, HttpStatus.OK );
         } else {
