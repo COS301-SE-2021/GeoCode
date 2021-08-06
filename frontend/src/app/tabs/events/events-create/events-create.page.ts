4fa98c0e45e987ba8b44131dec49c839463f1d89
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-08-06 14:21:50 +0200

    Added basic layout for create event page

--- frontend/src/app/tabs/events/events-create/events-create.page.ts
+++ frontend/src/app/tabs/events/events-create/events-create.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {GeoCode, GeoCodeService} from '../../../services/geocode-api';
+import {NavController} from '@ionic/angular';
+import {GoogleMapsLoader} from '../../../services/GoogleMapsLoader';
 
 @Component({
   selector: 'app-events-create',
   templateUrl: './events-create.page.html',
   styleUrls: ['./events-create.page.scss'],
 })
-export class EventsCreatePage implements OnInit {
+export class EventsCreatePage implements AfterViewInit  {
+  @ViewChild('mapElement',{static:false}) mapElement;
+  googleMaps;
+  mapOptions;
+  map;
+  mapMarker;
+  markers= [];
+  geocodes: GeoCode[] = [];
+  selected=[];
+  isHidden=true;
+  height='0%';
+  constructor(    private navCtrl: NavController,
+                  private geocodeApi: GeoCodeService,
+                  private mapsLoader: GoogleMapsLoader) { }
 
-  constructor() { }
+  //Create map and add mapmarkers of geocodes
+  loadMap(){
+    this.markers= [];
+    this.mapOptions = {
+      center: {lat: -25.75625115327836, lng: 28.235629260918344},
+      zoom: 15,
+    };
+    this.map = new this.googleMaps.Map(this.mapElement.nativeElement,this.mapOptions);
 
-  ngOnInit() {
   }
+
+  async ngAfterViewInit() {
+    this.googleMaps = await this.mapsLoader.load();
+    this.loadMap();
+  }
+
 
 }
