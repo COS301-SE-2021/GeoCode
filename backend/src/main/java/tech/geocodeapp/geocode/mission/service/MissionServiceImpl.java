831dc92b4ec18838ffc5a6e89f35346b1e74ce6e
Caleb Johnstone <u19030119@tuks.co.za>
2021-08-12 14:43:11 +0200

    getMissionById service implementation

--- backend/src/main/java/tech/geocodeapp/geocode/mission/service/MissionServiceImpl.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/service/MissionServiceImpl.java
@@ -2,16 +2,21 @@ package tech.geocodeapp.geocode.mission.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tech.geocodeapp.geocode.general.CheckNullRequestParameters;
 import tech.geocodeapp.geocode.general.exception.NullRequestParameterException;
+import tech.geocodeapp.geocode.mission.model.Mission;
 import tech.geocodeapp.geocode.mission.repository.MissionRepository;
 import tech.geocodeapp.geocode.mission.request.GetMissionByIdRequest;
 import tech.geocodeapp.geocode.mission.request.GetProgressRequest;
 import tech.geocodeapp.geocode.mission.response.GetMissionByIdResponse;
 import tech.geocodeapp.geocode.mission.response.GetProgressResponse;
 
+import java.util.Optional;
+
 @Service("MissionService")
 public class MissionServiceImpl implements MissionService{
     private final MissionRepository missionRepo;
+    private final CheckNullRequestParameters checkNullRequestParameters = new CheckNullRequestParameters();
 
     public MissionServiceImpl(MissionRepository missionRepo) {
         this.missionRepo = missionRepo;
@@ -24,7 +29,16 @@ public class MissionServiceImpl implements MissionService{
      */
     @Transactional
     public GetMissionByIdResponse getMissionById(GetMissionByIdRequest request) throws NullRequestParameterException {
-        return null;
+        if(request == null){
+            return new GetMissionByIdResponse(false, "The GetMissionByIdRequest object passed was NULL", null);
+        }
+
+        checkNullRequestParameters.checkRequestParameters(request);
+
+        Optional<Mission> optionalMission = missionRepo.findById(request.getMissionID());
+
+        return optionalMission.map(mission -> new GetMissionByIdResponse(true, "Mission found", mission)).orElseGet(
+                () -> new GetMissionByIdResponse(false, "Mission not found", null));
     }
 
     @Transactional
