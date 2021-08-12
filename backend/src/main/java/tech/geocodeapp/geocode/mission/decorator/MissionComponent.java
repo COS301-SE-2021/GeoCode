5735e7d015d95c083ff1942387c6e68267e89752
Liam <nitron.biohazard@gmail.com>
2021-08-12 15:42:07 +0200

    Created getId and setId methods

--- backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/MissionComponent.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/MissionComponent.java
@@ -1,4 +1,18 @@
 package tech.geocodeapp.geocode.mission.decorator;
 
+import java.util.UUID;
+
 public interface MissionComponent {
+
+    /**
+     * gets the id of a mission
+     * @return the UUID of the mission
+     */
+    UUID getId();
+
+    /**
+     *
+     * @param id the id
+     */
+    void setId(UUID id);
 }
