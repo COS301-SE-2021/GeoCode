ff91a1733ad7217855af4fdcf0a779d761243723
Liam <nitron.biohazard@gmail.com>
2021-08-13 23:44:17 +0200

    Implemented checkIfFinished

--- backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/SwapMission.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/SwapMission.java
@@ -5,4 +5,15 @@ public class SwapMission extends MissionDecorator{
     public SwapMission(MissionComponent decoratedMission) {
         super(decoratedMission);
     }
+
+    /**
+     * Compares the amount and completion to see if the correct number of swaps have been made
+     * @return the value returned from comparing if the completion and amount of a
+     * decorated mission are equal
+     */
+    @Override
+    public boolean checkIfFinished() {
+        return decoratedMission.getAmount().equals(decoratedMission.getCompletion());
+    }
+
 }
