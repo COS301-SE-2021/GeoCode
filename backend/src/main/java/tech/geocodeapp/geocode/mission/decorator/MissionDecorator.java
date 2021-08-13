fc57225a8d7640e4ff8d0609c34828886ecb7a3e
Liam <nitron.biohazard@gmail.com>
2021-08-13 20:34:59 +0200

    Implement method shells

--- backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/MissionDecorator.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/MissionDecorator.java
@@ -1,4 +1,57 @@
 package tech.geocodeapp.geocode.mission.decorator;
 
-public class MissionDecorator {
+import tech.geocodeapp.geocode.geocode.model.GeoPoint;
+
+import java.util.UUID;
+
+public abstract class MissionDecorator implements MissionComponent{
+    @Override
+    public UUID getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(UUID id) {
+
+    }
+
+    @Override
+    public Integer getAmount() {
+        return null;
+    }
+
+    @Override
+    public void setAmount(Integer amount) {
+
+    }
+
+    @Override
+    public GeoPoint getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(GeoPoint location) {
+
+    }
+
+    @Override
+    public Integer getCompletion() {
+        return null;
+    }
+
+    @Override
+    public void setCompletion(Integer completion) {
+
+    }
+
+    @Override
+    public void calculateDistance() {
+
+    }
+
+    @Override
+    public boolean checkIfFinished() {
+        return false;
+    }
 }
