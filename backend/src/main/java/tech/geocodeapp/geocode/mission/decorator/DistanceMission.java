1536cafb72f34f7990605146546e1c05d8b50054
Liam <nitron.biohazard@gmail.com>
2021-08-14 00:32:10 +0200

    Implemented calculateDistance

--- backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/DistanceMission.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/DistanceMission.java
@@ -1,8 +1,21 @@
 package tech.geocodeapp.geocode.mission.decorator;
 
+import tech.geocodeapp.geocode.geocode.model.GeoPoint;
+
 public class DistanceMission extends MissionDecorator{
 
     public DistanceMission(MissionComponent decoratedMission) {
         super(decoratedMission);
+    }
+
+    /**
+     * calculate the distance between the new location of a linked collectable and the previous location kept by
+     * the mission
+     * @param newLocation the location to compare
+     */
+    @Override
+    public void calculateDistance(GeoPoint newLocation) {
+        double distance = decoratedMission.getLocation().distanceTo(newLocation);
+        decoratedMission.setAmount(decoratedMission.getAmount() + ((int) distance));
     }
 }
