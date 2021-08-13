9325cffecc87193fe65d1f52323ae751dad9926e
Liam <nitron.biohazard@gmail.com>
2021-08-14 00:19:37 +0200

    Implemented checkLocation method

--- backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/GeoCodeMission.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/decorator/GeoCodeMission.java
@@ -1,8 +1,21 @@
 package tech.geocodeapp.geocode.mission.decorator;
 
+import tech.geocodeapp.geocode.geocode.model.GeoPoint;
+
 public class GeoCodeMission extends MissionDecorator{
 
     public GeoCodeMission(MissionComponent decoratedMission) {
         super(decoratedMission);
+    }
+
+    /**
+     * checks if the provided location is the same as the missions location and sets completion to 100 if it is
+     * @param newLocation the location to compare
+     */
+    @Override
+    public void checkLocation(GeoPoint newLocation) {
+        if(decoratedMission.getLocation().equals(newLocation)) {
+            decoratedMission.setCompletion(100);
+        }
     }
 }
