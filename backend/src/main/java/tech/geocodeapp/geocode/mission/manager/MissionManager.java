e72e55f64bb659f0e11d024f34df5f7bd2c5cf89
Liam <nitron.biohazard@gmail.com>
2021-08-14 11:25:32 +0200

    Created buildMission method

--- backend/src/main/java/tech/geocodeapp/geocode/mission/manager/MissionManager.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/manager/MissionManager.java
@@ -1,4 +1,12 @@
 package tech.geocodeapp.geocode.mission.manager;
 
+import tech.geocodeapp.geocode.mission.decorator.MissionComponent;
+import tech.geocodeapp.geocode.mission.model.Mission;
+
 public class MissionManager {
+
+
+    public MissionComponent buildMission(Mission mission) {
+
+    }
 }
