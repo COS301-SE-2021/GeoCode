65c5490190b3263a1be66851c7f135d1d1a7e27d
Liam <nitron.biohazard@gmail.com>
2021-08-14 10:58:55 +0200

    Created decorateMission method

--- backend/src/main/java/tech/geocodeapp/geocode/mission/factory/AbstractMissionFactory.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/factory/AbstractMissionFactory.java
@@ -1,4 +1,14 @@
 package tech.geocodeapp.geocode.mission.factory;
 
+import tech.geocodeapp.geocode.mission.decorator.MissionComponent;
+import tech.geocodeapp.geocode.mission.model.Mission;
+
 public abstract class AbstractMissionFactory {
+
+    /**
+     * @param mission the {@link Mission} to build the {@link MissionComponent}
+     * @param missionComponent the {@link MissionComponent} to decorate
+     * @return
+     */
+    public abstract MissionComponent decorateMission(Mission mission, MissionComponent missionComponent);
 }
