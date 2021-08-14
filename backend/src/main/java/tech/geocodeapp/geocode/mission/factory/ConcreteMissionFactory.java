c6a9d9575b1926e4d2ea5c46142cfba81ce0dc11
Liam <nitron.biohazard@gmail.com>
2021-08-14 11:08:38 +0200

    Implemented decorateMission

--- backend/src/main/java/tech/geocodeapp/geocode/mission/factory/ConcreteMissionFactory.java
+++ backend/src/main/java/tech/geocodeapp/geocode/mission/factory/ConcreteMissionFactory.java
@@ -1,4 +1,26 @@
 package tech.geocodeapp.geocode.mission.factory;
 
-public class ConcreteMissionFactory {
+import tech.geocodeapp.geocode.mission.decorator.ConcreteMission;
+import tech.geocodeapp.geocode.mission.decorator.MissionComponent;
+import tech.geocodeapp.geocode.mission.model.Mission;
+
+public class ConcreteMissionFactory extends AbstractMissionFactory{
+
+
+    /**
+     * sets all of the shared variables of a mission
+     * @param mission the {@link Mission} to build the {@link MissionComponent}
+     * @param missionComponent the {@link MissionComponent} to decorate
+     * @return
+     */
+    @Override
+    public MissionComponent decorateMission(Mission mission, MissionComponent missionComponent) {
+        MissionComponent toReturn = new ConcreteMission();
+        toReturn.setId(mission.getId());
+        toReturn.setAmount(mission.getAmount());
+        toReturn.setLocation(mission.getLocation());
+        toReturn.setCompletion(mission.getCompletion());
+
+        return toReturn;
+    }
 }
