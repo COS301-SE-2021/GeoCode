997b2b17fea4d7e97fbef9824a2b21b08f967e11
Liam <nitron.biohazard@gmail.com>
2021-08-18 16:18:59 +0200

    added createLeaderboardTestNullRequest

--- backend/src/test/java/tech/geocodeapp/geocode/leaderboard/LeaderboardServiceImplIT.java
+++ backend/src/test/java/tech/geocodeapp/geocode/leaderboard/LeaderboardServiceImplIT.java
@@ -1,10 +1,13 @@
 package tech.geocodeapp.geocode.leaderboard;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tech.geocodeapp.geocode.general.exception.NullRequestParameterException;
+import tech.geocodeapp.geocode.leaderboard.response.CreateLeaderboardResponse;
 import tech.geocodeapp.geocode.leaderboard.service.LeaderboardService;
 
 @SpringBootTest
@@ -13,5 +16,18 @@ public class LeaderboardServiceImplIT {
     @Autowired
     private LeaderboardService leaderboardService;
 
+
+    @Test
+    public void createLeaderboardTestNullRequest(){
+        try{
+            CreateLeaderboardResponse response = leaderboardService.createLeaderboard(null);
+
+            Assertions.assertFalse(response.isSuccess());
+            Assertions.assertEquals("The CreateLeaderboardRequest object passed was NULL", response.getMessage());
+            Assertions.assertNull(response.getLeaderboard());
+        }catch (NullRequestParameterException e){
+            Assertions.fail(e.getMessage());
+        }
+    }
 
 }
