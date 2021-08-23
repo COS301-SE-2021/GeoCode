b3018a5e63c7ea736ba88d2464ebc74bb32905d0
Caleb Johnstone <u19030119@tuks.co.za>
2021-08-23 19:51:40 +0200

    removed addTo repo functions that are no longer being used

--- backend/src/test/java/tech/geocodeapp/geocode/user/UserMockRepository.java
+++ backend/src/test/java/tech/geocodeapp/geocode/user/UserMockRepository.java
@@ -1,6 +1,5 @@
 package tech.geocodeapp.geocode.user;
 
-import org.junit.jupiter.api.Assertions;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,8 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import tech.geocodeapp.geocode.collectable.model.CollectableType;
-import tech.geocodeapp.geocode.geocode.model.GeoCode;
 import tech.geocodeapp.geocode.user.model.User;
 import tech.geocodeapp.geocode.user.repository.UserRepository;
 
@@ -140,81 +137,4 @@ public class UserMockRepository implements UserRepository {
         return false;
     }
 
-    /**
-     * Add GeoCode to the User's foundGeoCodes
-     *
-     * @param userID    UserID
-     * @param geocodeID GeoCode to add
-     */
-    @Override
-    public void addFoundGeoCode(UUID userID, UUID geocodeID) {
-        Optional<User> optionalUser = findById(userID);
-
-        Assertions.assertTrue(optionalUser.isPresent());
-        User user = optionalUser.get();
-
-        //no duplicates
-        if(user.getFoundGeocodes().stream().anyMatch(geocode -> geocode.getId().equals(geocodeID))){
-            return;
-        }
-
-        GeoCode geoCode = new GeoCode();
-        geoCode.setId(geocodeID);
-        user.addFoundGeocodesItem(geoCode);
-
-        map.put(userID, user);
-    }
-
-    /**
-     * Add CollectableType to the User's foundCollectableTypes
-     *
-     * @param userID            UserID
-     * @param collectableTypeID CollectableType to add
-     */
-    @Override
-    public void addFoundCollectableType(UUID userID, UUID collectableTypeID) {
-        Optional<User> optionalUser = findById(userID);
-
-        Assertions.assertTrue(optionalUser.isPresent());
-        User user = optionalUser.get();
-
-        //no duplicates
-        if(user.getFoundCollectableTypes().stream().anyMatch(type -> type.getId().equals(collectableTypeID))){
-            return;
-        }
-
-        CollectableType collectableType = new CollectableType();
-        collectableType.setId(collectableTypeID);
-        collectableType.setName("test CollectableType");
-        user.addFoundCollectableTypesItem(collectableType);
-
-        System.out.println("number of found collectable types:"+user.getFoundCollectableTypes().size());
-
-        map.put(userID, user);
-    }
-
-    /**
-     * Add GeoCode to the User's ownedGeoCodes
-     *
-     * @param userID    UserID
-     * @param geocodeID GeoCode to add
-     */
-    @Override
-    public void addOwnedGeoCode(UUID userID, UUID geocodeID) {
-        Optional<User> optionalUser = findById(userID);
-
-        Assertions.assertTrue(optionalUser.isPresent());
-        User user = optionalUser.get();
-
-        //no duplicates
-        if(user.getOwnedGeocodes().stream().anyMatch(geocode -> geocode.getId().equals(geocodeID))){
-            return;
-        }
-
-        GeoCode geoCode = new GeoCode();
-        geoCode.setId(geocodeID);
-        user.addOwnedGeocodesItem(geoCode);
-
-        map.put(userID, user);
-    }
 }
