fa994860fa9263f12a0ce77a23d5dfee59b21016
Liam <nitron.biohazard@gmail.com>
2021-08-26 15:15:48 +0200

    added BAD_REQUEST to deletePoint.

--- backend/src/main/java/tech/geocodeapp/geocode/leaderboard/controller/LeaderboardApiController.java
+++ backend/src/main/java/tech/geocodeapp/geocode/leaderboard/controller/LeaderboardApiController.java
@@ -82,7 +82,7 @@ public class LeaderboardApiController implements LeaderboardApi {
         if (response.isSuccess()) {
             return new ResponseEntity<>(response, HttpStatus.OK);
         }else{
-            return new ResponseEntity<>(response, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
     }
 
