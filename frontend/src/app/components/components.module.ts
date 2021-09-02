4b77677bf8a1e19eefd10e01b68a8288998af428
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-09-02 14:04:48 +0200

    Fix compilation errors

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -4,7 +4,6 @@ import {CommonModule} from '@angular/common';
 import {IonicModule} from '@ionic/angular';
 import {GeocodeDetailsComponent} from './geocode-details/geocode-details.component';
 import {FormsModule} from '@angular/forms';
-import {NavigationComponent} from './navigation/navigation.component';
 import {NgxBlocklyModule} from 'ngx-blockly';
 import {NavComponent} from './navigation/nav.component';
 import {NavHeaderComponent} from './navigation/elements/nav-header/nav-header.component';
@@ -17,7 +16,7 @@ import {NavigationLayoutsModule} from './navigation/layouts/navigation-layouts.m
     CommonModule,
     IonicModule,
     FormsModule,
-    NgxBlocklyModule
+    NgxBlocklyModule,
     FormsModule,
     NavigationLayoutsModule
   ],
