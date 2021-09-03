221bffa4c7605933222416c761affcc44b0d3cf7
Liam <nitron.biohazard@gmail.com>
2021-09-03 16:05:16 +0200

    Added constructor and blocklyDetails variable

--- backend/src/main/java/tech/geocodeapp/geocode/event/decorator/BlocklyEventDecorator.java
+++ backend/src/main/java/tech/geocodeapp/geocode/event/decorator/BlocklyEventDecorator.java
@@ -1,4 +1,13 @@
 package tech.geocodeapp.geocode.event.decorator;
 
-public class BlocklyEventDecorator {
+public class BlocklyEventDecorator extends EventDecorator{
+
+    private String blocklyDetails;
+
+    /**
+     * @param decoratedType the EventComponent to decorate
+     */
+    public BlocklyEventDecorator(EventComponent decoratedType) {
+        super(decoratedType);
+    }
 }
