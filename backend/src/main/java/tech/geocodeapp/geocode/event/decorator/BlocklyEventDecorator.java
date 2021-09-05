b0d038a1483326706250147e66cd5ed91b93babc
Liam <nitron.biohazard@gmail.com>
2021-09-05 14:42:16 +0200

    Implemented handleStageCompletion for when the totalBlocks can be divided by the total stages without remainder.

--- backend/src/main/java/tech/geocodeapp/geocode/event/decorator/BlocklyEventDecorator.java
+++ backend/src/main/java/tech/geocodeapp/geocode/event/decorator/BlocklyEventDecorator.java
@@ -1,5 +1,9 @@
 package tech.geocodeapp.geocode.event.decorator;
 
+import tech.geocodeapp.geocode.event.model.UserEventStatus;
+
+import java.util.*;
+
 public class BlocklyEventDecorator extends EventDecorator{
 
     private String blocklyDetails;
@@ -26,5 +30,35 @@ public class BlocklyEventDecorator extends EventDecorator{
     @Override
     public void setBlocklyDetails(String blocklyDetails) {
         this.blocklyDetails = blocklyDetails;
+    }
+
+
+    @Override
+    public void handleStageCompletion(int stageNumber, UserEventStatus status) {
+
+        //store number of stages and number of blocks
+        int totalStages = this.getGeocodeIDs().size();
+        var blocks = this.blocklyDetails.split("#");
+        int totalBlocks = blocks.length;
+
+        //check if blocks can be evenly spread across stages
+       if(totalBlocks % totalStages == 0) {
+           var blocksPerStage = totalBlocks/totalStages;
+           var count = 0;
+           while(count < blocksPerStage) {
+               var random = new Random().nextInt(totalBlocks);
+               List<String> foundBlocks = Arrays.asList(status.getDetails().get("blocklyDetails").split("#"));
+               if(!foundBlocks.contains(blocks[random])) {
+                   String userBlocks = status.getDetails().get("blocklyDetails");
+                   userBlocks += "#" + blocks[random];
+                   Map<String, String> temp = status.getDetails();
+                   temp.replace("blocklyDetails", userBlocks);
+                   status.setDetails(temp);
+                   count++;
+               }
+           }
+       }else{
+
+       }
     }
 }
