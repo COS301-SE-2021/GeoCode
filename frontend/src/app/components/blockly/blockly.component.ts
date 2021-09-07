bdf1a867d8b33a83b28d104ffadedd5b98fa1d93
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-09-08 00:47:33 +0200

    Support running js programs and save blockly default toolbox from demo

--- frontend/src/app/components/blockly/blockly.component.ts
+++ frontend/src/app/components/blockly/blockly.component.ts
@@ -1,7 +1,9 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {NgxBlocklyComponent} from 'ngx-blockly';
 import * as Blockly from 'blockly';
 import * as BlocklyJsGenerator from 'blockly/javascript';
+import Interpreter from 'js-interpreter';
+import {GeoCode} from '../../services/geocode-api';
 
 @Component({
   selector: 'app-blockly',
@@ -9,6 +11,8 @@ import * as BlocklyJsGenerator from 'blockly/javascript';
   styleUrls: ['./blockly.component.scss'],
 })
 export class BlocklyComponent implements AfterViewInit {
+
+  @Input() toolboxBlocks: string[] = null;
 
   @ViewChild('blocklyDiv', {static:false}) blocklyDiv: ElementRef<HTMLDivElement>;
   //@ViewChild('ngxBlockly', {static:false}) ngxBlockly: NgxBlocklyComponent;
@@ -27,29 +31,80 @@ export class BlocklyComponent implements AfterViewInit {
       drag: true,
       wheel: true
     },
-    toolbox: this.toolbox
+    toolbox: this.toolbox,
   };
 
   constructor() {}
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
+    if (this.toolboxBlocks !== null) {
+      await this.readInputToolbox();
+    } else {
+      await this.readDefaultToolbox();
+    }
     this.workspace = Blockly.inject(this.blocklyDiv.nativeElement, this.config);
+    console.log(this.workspace);
+    console.log(this.workspace.isVisible());
     //this.workspace = this.ngxBlockly.workspace;
-    this.addToToolbox('controls_if');
-    this.addToToolbox('controls_whileUntil');
+    //window.dispatchEvent(new Event('resize'));
+    setTimeout(() => window.dispatchEvent(new Event('resize')), 500);
   }
 
-  addToToolbox(blockName: string) {
-    // @ts-ignore use pre-made blocks, no need to define them ourselves
-    this.toolbox.contents.push({
-      kind: 'block',
-      type: blockName
+  runProgram() {
+    const code = this.generateCode();
+    console.log(code);
+    const interpreter = new Interpreter(code, (interpreter2, scope) => {
+      const alertWrapper = (text) => {
+        text = text ? text.toString() : '';
+        alert(text);
+      };
+      interpreter2.setProperty(scope, 'alert', interpreter2.createNativeFunction(alertWrapper));
+      const promptWrapper = (text) => {
+        text = text ? text.toString() : '';
+        prompt(text);
+      };
+      interpreter2.setProperty(scope, 'prompt', interpreter2.createNativeFunction(promptWrapper));
     });
-    this.workspace.updateToolbox(this.toolbox);
+
+    let stepsAllowed = 10000;
+    while (interpreter.step() && stepsAllowed) {
+      stepsAllowed--;
+    }
+    if (!stepsAllowed) {
+      throw EvalError('Infinite loop.');
+    }
   }
 
   generateCode(): string {
     return BlocklyJsGenerator.workspaceToCode(this.workspace);
+  }
+
+  async readInputToolbox() {
+    for (const blockName of this.toolboxBlocks) {
+      // @ts-ignore
+      this.toolbox.contents.push({
+        kind: 'block',
+        type: blockName
+      });
+    }
+  }
+
+  async readDefaultToolbox() {
+    return new Promise<void>((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      req.open('GET', '/assets/blockly-default-blocks.xml', false);
+      req.onreadystatechange = () => {
+        if(req.readyState === 4){
+          if(req.status === 200 || req.status === 0) {
+            this.config.toolbox = req.responseText;
+            resolve();
+          } else {
+            reject();
+          }
+        }
+      };
+      req.send(null); // return control
+    });
   }
 
 }
