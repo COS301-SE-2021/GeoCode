85033e024658076528324e42457ba398e1a87e4c
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-09-10 15:50:10 +0200

    Added hint alert

--- frontend/src/app/tabs/profile/geocodes/geocode-update/geocode-update.component.ts
+++ frontend/src/app/tabs/profile/geocodes/geocode-update/geocode-update.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {GeoCodeService, UpdateGeoCodeRequest, UpdateGeoCodeResponse} from '../../../../services/geocode-api';
+import {AlertController} from "@ionic/angular";
 
 @Component({
   selector: 'app-geocode-update',
@@ -7,6 +8,7 @@ import {GeoCodeService, UpdateGeoCodeRequest, UpdateGeoCodeResponse} from '../..
   styleUrls: ['./geocode-update.component.scss'],
 })
 export class GeocodeUpdateComponent implements OnInit {
+  geocode;
   updateRequest: UpdateGeoCodeRequest ={
     available: false,
     description: '',
@@ -15,9 +17,16 @@ export class GeocodeUpdateComponent implements OnInit {
     hints: [],
     location: {latitude:0,longitude:0}
   };
-  constructor(private geocodeAPI: GeoCodeService) { }
+  constructor(private geocodeAPI: GeoCodeService, private alertCtrl: AlertController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updateRequest.available = this.geocode.available;
+    this.updateRequest.difficulty = this.geocode.difficulty;
+    this.updateRequest.hints = this.geocode.hints;
+    this.updateRequest.geoCodeID= this.geocode.id;
+    this.updateRequest.location=this.geocode.location;
+    this.updateRequest.description = this.geocode.description;
+  }
 
   updateDescription($event){
     this.updateRequest.description = $event.detail.value;
@@ -32,9 +41,46 @@ export class GeocodeUpdateComponent implements OnInit {
   }
 
   updateGeoCode(){
+
     this.geocodeAPI.updateGeoCode(this.updateRequest).subscribe((response: UpdateGeoCodeResponse)=>{
       console.log(response);
     });
+  }
+
+  async addHint() {
+
+    const alert = this.alertCtrl.create({
+      header: 'Add Hint',
+      message: 'Please enter a hint description',
+      inputs: [
+        {
+          name: 'hint',
+          placeholder: 'hint'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            if (data.hint === '') {
+
+            } else {
+              console.log(data.hint);
+              this.updateRequest.hints.push(data.hint);
+            }
+          }
+        }
+      ]
+    });
+    (await alert).present();
+
   }
 
 }
