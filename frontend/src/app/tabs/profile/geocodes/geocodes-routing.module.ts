90022e6b3d1117a0e966e2034690112c81f35254
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-09-10 22:40:38 +0200

    Declare geocode update component in module

--- frontend/src/app/tabs/profile/geocodes/geocodes-routing.module.ts
+++ frontend/src/app/tabs/profile/geocodes/geocodes-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes),CommonModule],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class UserGeocodesPageRoutingModule {}
