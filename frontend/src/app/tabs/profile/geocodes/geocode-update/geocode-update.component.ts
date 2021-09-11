b83fc6e59a32c833f19758cca69f8e28b473a5ea
JennaLynGallagher <72206576+JennaLynGallagher@users.noreply.github.com>
2021-09-11 10:32:42 +0200

    Added styling

--- frontend/src/app/tabs/profile/geocodes/geocode-update/geocode-update.component.ts
+++ frontend/src/app/tabs/profile/geocodes/geocode-update/geocode-update.component.ts
@@ -42,7 +42,6 @@ export class GeocodeUpdateComponent implements OnInit {
   }
 
   updateGeoCode(){
-
     this.geocodeAPI.updateGeoCode(this.updateRequest).subscribe((response: UpdateGeoCodeResponse)=>{
       console.log(response);
     });
