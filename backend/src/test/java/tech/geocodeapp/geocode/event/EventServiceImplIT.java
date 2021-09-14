98c3f9110376703c10edd9a86530aa2de4d25788
Liam <nitron.biohazard@gmail.com>
2021-09-14 15:02:22 +0200

    Removed test for ChangeAvailability as the method is commented out for the success path

--- backend/src/test/java/tech/geocodeapp/geocode/event/EventServiceImplIT.java
+++ backend/src/test/java/tech/geocodeapp/geocode/event/EventServiceImplIT.java
@@ -645,28 +645,6 @@ class EventServiceImplIT {
                 .hasMessageContaining( reqParamError );
     }
 
-    /**
-     * Using valid data does the changeAvailability use case test
-     * complete successfully
-     */
-    @Test
-    @Order( 7 )
-    @DisplayName( "Valid request - changeAvailability" )
-    void changeAvailabilityTest() {
-
-        try {
-            //ToDo finish this
-            ChangeAvailabilityRequest request = new ChangeAvailabilityRequest();
-            request.setAvailability( null );
-            request.setEventID( null );
-
-            var event = eventService.changeAvailability( request );
-        } catch ( InvalidRequestException e ) {
-
-            /* An error occurred, print the stack to identify */
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Check how the use case handles the request being null
